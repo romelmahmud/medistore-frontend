153f4e75d3a72d79dcd8255dd4f58da802332b26
romel <romelmahmud.dev@gmail.com>
2026-02-24 21:26:17 +0600

    refactor for cookie problem

--- src/env.ts
+++ src/env.ts
@@ -1,28 +1,27 @@
-// src/env.ts
 import { createEnv } from "@t3-oss/env-nextjs";
 import * as z from "zod";
 
 export const env = createEnv({
-  // Server-only environment variables
   server: {
     BACKEND_URL: z.string().url(),
     FRONTEND_URL: z.string().url(),
   },
 
-  // Variables accessible in the browser (must start with NEXT_PUBLIC_)
   client: {
+    NEXT_PUBLIC_BACKEND_URL: z.string().url(),
     NEXT_PUBLIC_API_URL: z.string().url(),
-    NEXT_PUBLIC_AUTH_URL: z.string().url(),
+
     NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: z.string(),
     NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET: z.string(),
   },
 
-  // Runtime environment variable mapping
   runtimeEnv: {
     BACKEND_URL: process.env.BACKEND_URL,
     FRONTEND_URL: process.env.FRONTEND_URL,
+
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    NEXT_PUBLIC_AUTH_URL: process.env.NEXT_PUBLIC_AUTH_URL,
+
     NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME:
       process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
     NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET:
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,8 @@
-import { env } from "@/env";
 import { createAuthClient } from "better-auth/react";
-console.log(env.NEXT_PUBLIC_AUTH_URL);
+
 export const authClient = createAuthClient({
-  baseURL: env.NEXT_PUBLIC_AUTH_URL,
-  credentials: "include", // ✅ must have this
+  baseURL: typeof window !== "undefined" ? window.location.origin : "",
+  fetchOptions: {
+    credentials: "include",
+  },
 });
